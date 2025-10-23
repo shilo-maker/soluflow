74de91a1916475104f828555dbdde2178c62c32f
Shilo <shilo@soluisrael.org>
2025-10-23 04:03:50 +0300

    Fix Express 5 compatibility: change wildcard route from '*' to '/*'

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(clientBuildPath));
 
   // Handle React routing, return all requests to React app
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(clientBuildPath, 'index.html'));
   });
 } else {
